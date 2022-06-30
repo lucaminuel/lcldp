a8a6f3e42bc732bf634135ac817e90d1b15cd5ef
Manuel Luci <m.luci@studenti.unipi.it>
2022-06-30 23:03:32 +0200

    unit test

--- test/__init__.py
+++ test/__init__.py
@@ -1,3 +1,2 @@
-from test_data_visualization import *
-from test_load_info_data import *
-from test_xgboost_tool import *
+import os, sys; sys.path.append(os.path.dirname(os.path.realpath(__file__)))
+__all__ =['test_data_visualization', 'test_load_info_data', 'test_xgboost_tool']
