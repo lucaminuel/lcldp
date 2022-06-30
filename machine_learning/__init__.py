1cac183644c1dc3586bb22e77152f164d6576f3e
Manuel Luci <m.luci@studenti.unipi.it>
2022-06-30 22:54:03 +0200

    unit test

--- machine_learning/__init__.py
+++ machine_learning/__init__.py
@@ -1,5 +1,1 @@
-import os, sys; sys.path.append(os.path.dirname(os.path.realpath(__file__)))
-from machine_learning.xgboost_tool import basic_xgboost, hyper_xgboost
-from machine_learning.neural_network_tool import basic_neural_network, test_neural_network, dropout_neural_network, diamond_neural_network, hyper_neural_network
-
-
+__all__ = ["sgboost_tool, neural_network_tool"]
---
+++ test/__init__.py
@@ -0,0 +1,3 @@
+from test_data_visualization import *
+from test_load_info_data import *
+from test_xgboost_tool import *
