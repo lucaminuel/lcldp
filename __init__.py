89e21172e7bee04e9c1afb0f70b43490965464b2
Manuel Luci <m.luci@studenti.unipi.it>
2022-06-30 18:50:21 +0200

    init

---
+++ __init__.py
@@ -0,0 +1,4 @@
+from data_analysis.load_info_data import *
+from data_analysis.plot_image import *
+from machine_learning.xgboost_tool import *
+from machine_learning.neural_network_tool import *
--- data_analysis/__init__.py
+++ data_analysis/__init__.py
@@ -1,4 +1,4 @@
 import os, sys; sys.path.append(os.path.dirname(os.path.realpath(__file__)))
-from data_analysis.load_info_data import load_info_data
-from data_analysis.plot_image import countpl, barpl, histpl, corr_matrix
+from load_info_data import *
+from data_analysis.plot_image import *
 
