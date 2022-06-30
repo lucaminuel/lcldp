4f4653ab703f431d45ba166cd097c42bc87eedaf
Manuel Luci <m.luci@studenti.unipi.it>
2022-06-30 04:46:03 +0200

    init

---
+++ machine_learning/__init__.py
@@ -0,0 +1,5 @@
+import os, sys; sys.path.append(os.path.dirname(os.path.realpath(__file__)))
+from machine_learning.xgboost_tool import basic_xgboost, hyper_xgboost
+from machine_learning.neural_network_tool import basic_neural_network, test_neural_network, dropout_neural_network, diamond_neural_network, hyper_neural_network
+
+
