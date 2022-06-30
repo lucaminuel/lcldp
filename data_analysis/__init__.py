81a2a6b6c2c6363daed26251d886ee43addc6641
Manuel Luci <m.luci@studenti.unipi.it>
2022-06-30 04:34:18 +0200

    setup file

---
+++ data_analysis/__init__.py
@@ -0,0 +1,3 @@
+from data_analysis.load_info_data import load_info_data
+from data_analysis.plot_image import countpl, barpl, histpl, corr_matrix
+
---
+++ setup.py
@@ -0,0 +1,5 @@
+from setuptools import setup, find_packages
+setup(
+    name = 'load_info_data',
+    packages = find_packages(),
+)
