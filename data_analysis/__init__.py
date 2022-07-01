544022e13de48a53d1547680714d0ac93a18bf29
Manuel Luci <m.luci@studenti.unipi.it>
2022-07-01 15:44:26 +0200

    init file

--- data_analysis/__init__.py
+++ data_analysis/__init__.py
@@ -1,2 +1,2 @@
-import os, sys; sys.path.append(os.path.dirname(os.path.realpath(__file__)))
-__all__ = ['plot_image', 'load_info_data']
+#import os, sys; sys.path.append(os.path.dirname(os.path.realpath(__file__)))
+#__all__ = ['plot_image', 'load_info_data']
--- machine_learning/__init__.py
+++ machine_learning/__init__.py
@@ -1,1 +1,2 @@
-__all__ = ["xgboost_tool", "neural_network_tool"]
+import os, sys; sys.path.append(os.path.dirname(os.path.realpath(__file__)))
+__all__ = ['xgboost_tool', 'neural_network_tool']
